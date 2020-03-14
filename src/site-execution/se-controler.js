770b7055a857762619b2f95979c240166ae6e3cb
GustavoMinghini <45168589+GustavoMinghini@users.noreply.github.com>
2020-03-14 09:54:07 -0300

    controler pre-feito

--- src/site-execution/se-controler.js
+++ src/site-execution/se-controler.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const service = require('./se-service')
 
-router.post('/', async (req, res) => {
-    
-    res.json()
-})
+const executeRequest = (req, res) => {
+
+    service.execute(new SiteRequestModel(data))
+        .then(response => res.json(response))
+        .catch(error => res.send(error))
+}
+
+
+router.post('/',  async (req, res) => executeRequest(req.body, res))
+
 
 module.exports = router
